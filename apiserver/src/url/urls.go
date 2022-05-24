a10a9884dc5d0d7aa116695193285c5caccac8ab
Vectorxj <Vectorxj@outlook.com>
2022-05-24 13:26:58 +0800

    code: fix url

--- apiserver/src/url/urls.go
+++ apiserver/src/url/urls.go
@@ -2,7 +2,7 @@ package url
 
 const (
 	HttpScheme = "http://"
-	Hostname   = "localhost"
+	Hostname   = "10.119.11.101"
 	Port       = ":8080"
 	Prefix     = HttpScheme + Hostname + Port
 
@@ -40,7 +40,7 @@ const (
 	DNSURL = "/api/v1/dns/"
 
 	EndpointURL = "/endpoint/"
-	GpuURL = "/api/v1/gpu/"
+	GpuURL      = "/api/v1/gpu/"
 
 	ResetURL = "/reset"
 )
